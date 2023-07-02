6e18697c040f1a85d2595014907e9d964e8423ba
GuilhermeSams <guilhermeps3670@gmail.com>
2023-07-02 03:56:04 -0300

    created project, instaled and configured shadcn.ui components and prototype navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from "@/components/navbar"
 
 export const metadata: Metadata = {
   title: {
@@ -36,13 +36,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <head />
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "min-h-screen bg-white font-sans antialiased",
             fontSans.variable
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
+              <Navbar />
               <div className="flex-1">{children}</div>
             </div>
             <TailwindIndicator />
