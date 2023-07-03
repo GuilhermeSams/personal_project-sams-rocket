d74fafe80e4c93b6bcd11a013335b5fd9b3f73ac
GuilhermeSams <guilhermeps3670@gmail.com>
2023-07-03 00:25:43 -0300

    section background image and section title

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="pt-br" suppressHydrationWarning>
         <head />
-        <body className="bg-white">
+        <body className="max-w-full bg-white">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
               <Navbar />
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { Share_Tech_Mono } from 'next/font/google'
+import Image from 'next/image'
+import img_bg from 'public/rocketbg.jpg'
+
+const techfont = Share_Tech_Mono({
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
 export default function Home() {
   return (
-    <>
-      <h1>Oi</h1>
-    </>
+    <main className={techfont.className}>
+      <section className="relative">
+        <div className="absolute z-[-1] h-screen w-screen max-w-full">
+          <Image
+            alt="img_rocket"
+            src={img_bg}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+              filter: 'brightness(0.4)',
+            }}
+          />
+        </div>
+        <div className="flex h-screen items-center justify-center">
+          <h1 className="mx-3 w-[50rem] text-center text-4xl leading-relaxed">
+            Porque investir no setor aeroespacial, se ainda temos enormes
+            problemas na Terra?
+          </h1>
+        </div>
+      </section>
+    </main>
   )
 }
