72c11fe42a2b587e9e798dab850f62edf5069665
GuilhermeSams <guilhermeps3670@gmail.com>
2023-07-22 01:22:38 -0300

    fix error to deploy

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 
 import { Icons } from '@/components/icons'
 
+import { sharetechmono } from '../lib/fonts'
 import navbarDataCompany from './navbar/navbarDataCompany'
 import navbarDataTransmition from './navbar/navbarDataTransmition'
 
@@ -11,85 +12,87 @@ export default function Footer() {
   const dataTransmition = navbarDataTransmition()
   const dataCompany = navbarDataCompany()
   return (
-    <div className="bg-gray-300 bg-gradient-to-b from-white via-gray-400 to-gray-600 ">
-      <div className="flex items-start justify-evenly py-12 text-black">
-        <div className="mx-8 flex flex-col items-center gap-3 sm:flex-row">
-          <a
-            className="scale-100 transition-none duration-300 md:transition md:delay-100 md:ease-in-out md:hover:scale-125"
-            href="https://www.linkedin.com/in/guilherme-pereira-sampaio/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icons.linkedin />
-          </a>
-          <a
-            className="scale-100 transition-none duration-300 md:transition md:delay-100 md:ease-in-out md:hover:scale-125"
-            href="https://github.com/GuilhermeSams"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icons.github />
-          </a>
-        </div>
-        <div className="flex flex-wrap gap-x-32 gap-y-2 max-[431px]:flex-col">
-          <div>
-            <h2 className="text-lg">Foguetes</h2>
-            <div className="flex flex-col  text-base text-gray-900">
-              <Link
-                className="my-2 hover:text-black"
-                href={'/foguetes/starship'}
-              >
-                Starship
-              </Link>
-              <Link
-                className="my-2 hover:text-black"
-                href={'/foguetes/falcon9'}
-              >
-                Falcon 9
-              </Link>
-              <Link
-                className="my-2 hover:text-black"
-                href={'/foguetes/spaceshuttle'}
-              >
-                Space Shuttle
-              </Link>
-              <Link
-                className="my-2 hover:text-black"
-                href={'/foguetes/ariane5'}
-              >
-                Ariane 5
-              </Link>
+    <div className={sharetechmono.className}>
+      <div className="bg-gray-300 bg-gradient-to-b from-white via-gray-400 to-gray-600 ">
+        <div className="flex items-start justify-evenly py-12 text-black">
+          <div className="mx-8 flex flex-col items-center gap-3 sm:flex-row">
+            <a
+              className="scale-100 transition-none duration-300 md:transition md:delay-100 md:ease-in-out md:hover:scale-125"
+              href="https://www.linkedin.com/in/guilherme-pereira-sampaio/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icons.linkedin />
+            </a>
+            <a
+              className="scale-100 transition-none duration-300 md:transition md:delay-100 md:ease-in-out md:hover:scale-125"
+              href="https://github.com/GuilhermeSams"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icons.github />
+            </a>
+          </div>
+          <div className="flex flex-wrap gap-x-32 gap-y-2 max-[431px]:flex-col">
+            <div>
+              <h2 className="text-lg">Foguetes</h2>
+              <div className="flex flex-col  text-base text-gray-900">
+                <Link
+                  className="my-2 hover:text-black"
+                  href={'/foguetes/starship'}
+                >
+                  Starship
+                </Link>
+                <Link
+                  className="my-2 hover:text-black"
+                  href={'/foguetes/falcon9'}
+                >
+                  Falcon 9
+                </Link>
+                <Link
+                  className="my-2 hover:text-black"
+                  href={'/foguetes/spaceshuttle'}
+                >
+                  Space Shuttle
+                </Link>
+                <Link
+                  className="my-2 hover:text-black"
+                  href={'/foguetes/ariane5'}
+                >
+                  Ariane 5
+                </Link>
+              </div>
+            </div>
+            <div>
+              <h1 className="text-lg">Transmisões</h1>
+              <ul className="flex flex-col text-base text-gray-900 ">
+                {dataTransmition.map((item) => (
+                  <li key={item.id} className="my-2">
+                    <Link className="hover:text-black" href={item.href}>
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h1 className="text-lg">Empresas</h1>
+              <ul className="flex flex-col text-base text-gray-900">
+                {dataCompany.map((item) => (
+                  <li key={item.id} className="my-2">
+                    <Link className="hover:text-black" href={item.href}>
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div>
-            <h1 className="text-lg">Transmisões</h1>
-            <ul className="flex flex-col text-base text-gray-900 ">
-              {dataTransmition.map((item) => (
-                <li key={item.id} className="my-2">
-                  <Link className="hover:text-black" href={item.href}>
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h1 className="text-lg">Empresas</h1>
-            <ul className="flex flex-col text-base text-gray-900">
-              {dataCompany.map((item) => (
-                <li key={item.id} className="my-2">
-                  <Link className="hover:text-black" href={item.href}>
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
         </div>
+        <p className="pb-5 text-center font-semibold text-black">
+          Created by GuiSams
+        </p>
       </div>
-      <p className="pb-5 text-center font-semibold text-black">
-        Created by GuiSams
-      </p>
     </div>
   )
 }
