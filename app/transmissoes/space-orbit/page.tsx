46e983d1ca81ef5dbce3dc4b38b5415906de3543
GuilhermeSams <guilhermeps3670@gmail.com>
2023-10-25 12:20:56 -0300

    consertando problema no card space orbit

--- app/transmissoes/space-orbit/page.tsx
+++ app/transmissoes/space-orbit/page.tsx
@@ -10,8 +10,8 @@ export default function SpaceOrbit() {
     <main className="mt-14 h-full max-w-full bg-gradient-to-t from-white via-gray-400 to-gray-500 pt-8 text-black">
       <div className={sharetechmono.className}>
         <div className="mx-auto flex w-4/5  flex-col items-center justify-center space-y-10 rounded-2xl border-[1px] border-solid border-blue-800 bg-gray-300 text-center shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]">
-          <div className="h-3/6 w-full rounded-t-2xl bg-blue-800 pb-4 pt-10">
-            <div className="mx-auto h-40 w-40 rounded-full bg-white">
+          <div className="h-3/6 w-full pb-4 pt-10">
+            <div className="mx-auto h-40 w-40 rounded-full">
               <Image
                 className="rounded-full bg-black shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
                 src={profile_pedro_pallota}
@@ -61,7 +61,7 @@ export default function SpaceOrbit() {
         <div className="mt-12">
           <div className="mx-auto w-2/4 animate-fade-up text-center">
             <p className="rounded px-4 py-2 text-lg shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
-              Análise logo após a conclusão do teste de voo da Starship
+              Teste de voo da Starship
             </p>
           </div>
           <div className="mx-auto animate-fade-up py-4 animate-delay-300">
